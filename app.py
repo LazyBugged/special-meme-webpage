366b18eafbd1482d09f850722d361e663fc6b35b
Max Base <MaxBaseCode@Gmail.Com>
2021-11-02 15:55:51 +0330

    Create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+# https://github.com/LazyBugged
+# 11/02/2021
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'LazyBugged!'
+
+app.run(host='0.0.0.0', port=81)
