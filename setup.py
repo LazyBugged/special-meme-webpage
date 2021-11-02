1e753ef15bf337743b98be71497d75d8022db5ab
Max Base <MaxBaseCode@Gmail.Com>
2021-11-02 15:58:26 +0330

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1' 
+DESCRIPTION = 'LazyBugged'
+LONG_DESCRIPTION = 'Python package'
+
+setup(
+        name="lazybugged", 
+        version=VERSION,
+        author="Max Base and ...",
+        author_email="<youremail@email.com>",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=[],
+        keywords=['python', 'first package'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ]
+)
